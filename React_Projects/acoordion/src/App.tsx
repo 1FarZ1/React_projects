02c437168fcd6afd42b1677d0b71c2afe4bc7346
1FarZ1 <91225280+1FarZ1@users.noreply.github.com>
2023-06-17 10:23:05 +0100

    complted acoordion

--- React_Projects/acoordion/src/App.tsx
+++ React_Projects/acoordion/src/App.tsx
@@ -1,13 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import questions from './data.ts'
+import {useState} from 'react'
 import './App.css'
 
 function App() {
+  const [shown,setShown] = useState(false);
+
  return (
-  <>
-  <h1>acoordion</h1>
-  </>
+  <main>
+      <div className="container">
+        <h1>
+          Questions
+        </h1>
+
+       {
+        questions.map((elm)=>{
+          return <div className='question' key={elm.id}>
+          <header>
+             <h5>{elm.title}</h5>
+             <button  onClick={()=>setShown(!shown)} className="question-btn">
+            <span className="plus-icon"/>
+          </button>
+          </header>
+ 
+          {
+                 shown &&          <p>{elm.info}</p>
+            
+          }
+   
+ 
+ 
+ 
+         </div>
+        })
+       }
+      </div>
+  </main>
  )
 }
 
