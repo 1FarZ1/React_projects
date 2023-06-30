3105823f2d067f1777574fdf69c35de30c489a6f
1FarZ1 <91225280+1FarZ1@users.noreply.github.com>
2023-06-30 21:59:37 +0100

    checkout done

--- React_Projects/16.comfy_sloth/src/pages/CheckOut.tsx
+++ React_Projects/16.comfy_sloth/src/pages/CheckOut.tsx
@@ -1,7 +1,18 @@
 import React from 'react'
+import styled from 'styled-components'
+// import { PageHero, StripeCheckout } from '../components'
+// extra imports
+import { useCartContext } from '../context/cart_context'
+import { Link } from 'react-router-dom'
+import PageHero from '../components/PageHero'
 
-export default function CheckOut() {
-  return (
-    <div>CheckOut</div>
-  )
+const CheckoutPage = () => {
+  return <main>
+    <PageHero title= "Checkout"/>
+    <Wrapper className="page section section-center">
+      <h1>checkout here</h1>
+      </Wrapper>
+  </main>
 }
+const Wrapper = styled.div``
+export default CheckoutPage
--- React_Projects/16.comfy_sloth/src/pages/Product.tsx
+++ React_Projects/16.comfy_sloth/src/pages/Product.tsx
@@ -1,7 +1,30 @@
 import React from 'react'
+import styled from 'styled-components'
+import PageHero from '../components/PageHero'
+// import { Filters, ProductList, Sort, PageHero } from '../components'
 
-export default function Product() {
-  return (
-    <div>Product</div>
-  )
+const ProductsPage = () => {
+  return <main>
+    <PageHero title="products"/>
+    <Wrapper className="page section section-center">
+        test
+        {/* <PageHero title="products" /> */}
+        {/* <div className="section-center products">*/}
+        </Wrapper>   
+  </main>
 }
+
+const Wrapper = styled.div`
+  .products {
+    display: grid;
+    gap: 3rem 1.5rem;
+    margin: 4rem auto;
+  }
+  @media (min-width: 768px) {
+    .products {
+      grid-template-columns: 200px 1fr;
+    }
+  }
+`
+
+export default ProductsPage
