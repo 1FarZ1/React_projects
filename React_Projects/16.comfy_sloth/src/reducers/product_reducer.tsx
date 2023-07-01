5cdb6c3a41a98f522062434846bd90b40447428f
1FarZ1 <91225280+1FarZ1@users.noreply.github.com>
2023-07-01 10:54:58 +0100

    adding the actions to reducer

--- React_Projects/16.comfy_sloth/src/reducers/product_reducer.tsx
+++ React_Projects/16.comfy_sloth/src/reducers/product_reducer.tsx
@@ -8,5 +8,11 @@ export const Productreducer = (state:any,action:any)=>{
             return {...state,isSideBarOpen:true};
         case ActionType.CLOSE_SIDEBAR:
             return {...state,isSideBarOpen:false};
+        case ActionType.GET_PRODUCTS_BEGIN:
+            return {...state,products_loading:true};
+        case ActionType.GET_PRODUCTS_SUCCESS:
+            return {...state,products_loading:false,products:action.payload};
+        case ActionType.GET_PRODUCTS_ERROR:
+            return {...state,products_loading:false,products_error:true};
     }
 }
